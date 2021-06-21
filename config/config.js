026ed32acbfb1c82832f8f806389119edbec7231
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-21 11:29:18 +0300

    del config

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const mongoDbString =
-  'mongodb+srv://mariusAdmin:secret1234@frankfurtclusteraws.ambmk.mongodb.net/reactDB?retryWrites=true&w=majority';
-
-// <password> - pakeisti i slaptazodi
-// myFirstDatabase - pakeisti i db pavadinima
-
-module.exports = {
-  mongoDbString,
-};
