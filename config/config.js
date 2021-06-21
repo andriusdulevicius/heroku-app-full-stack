630536e3167085195b3182261a8b3c95a5f6cfd1
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-21 11:02:48 +0300

    new file structure

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const mongoDbString =
+  'mongodb+srv://mariusAdmin:secret1234@frankfurtclusteraws.ambmk.mongodb.net/reactDB?retryWrites=true&w=majority';
+
+// <password> - pakeisti i slaptazodi
+// myFirstDatabase - pakeisti i db pavadinima
+
+module.exports = {
+  mongoDbString,
+};
